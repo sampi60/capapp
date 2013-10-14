25ade082fd6a42218e3f889537c7b28d0da04ba8
Piotr Sampławski <sampi@gumed.edu.pl>
2013-10-14 14:44:17 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capapp::Application.config.secret_token = '4e4d9754d5b9c1d1bc2c282cf975a99fbe5d63d090df242e8d2b84a825552e33397deadef3edef94beddcffafb45f403e276ee3c1c3b824c9b959afe89272164'
