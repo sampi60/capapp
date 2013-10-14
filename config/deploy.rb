636b9b05f01a318a20efcfe1d7ae799c11b12ee8
Piotr Sampławski <sampi@gumed.edu.pl>
2013-10-14 15:29:32 +0200

    konfiguracja Capistrano dla alwaysdata

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,31 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "capapp"
+set :user, "sampi"
+set :deploy_to, "/home/#{user}/www/#{application}"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+set :repository, "git@code.gumed.edu.pl:#{user}/#{application}.git"
+set :branch, 'cap2'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :use_sudo, false
+ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
+
+
+set :domain, "ssh.alwaysdata.com"
+server domain, :app, :web
+role :db, domain, :primary => true
+set :runner, user
+set :keep_releases, 5
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
